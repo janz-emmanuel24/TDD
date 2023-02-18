0afcd54600aee92530a72605e3a1f5de90474607
Jansuk Emmanuel <jansabbas1@gmail.com>
2023-02-19 00:57:52 +0300

    RED: Fail, not the desired result

--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@ import { assert } from "chai";
 import { multiply } from "../src/multiply";
 
 describe('Testing the multiply function', () =>{
-    it('Tests the return value of multiplying two numbers', () => {
+    it('Tests the return value of multiplying 3,5', () => {
         assert.equal(multiply(3,5), 15, 'The output is wrong')
+    })
+
+    it('Tests the return value of multiplying 2,2', () => {
+        assert.equal(multiply(2,2), 15, 'The output of multiplying 2 * 2 is 4')
     })
 })
