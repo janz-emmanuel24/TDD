e0f82b82c158d13eb0c3f1f9f264f888bc4434f8
Jansuk Emmanuel <jansabbas1@gmail.com>
2023-02-19 01:04:35 +0300

    RED: Fail, not the expected result

--- test/test.js
+++ test/test.js
@@ -13,4 +13,8 @@ describe('Testing the multiply function', () =>{
     it('Tests the return value of multiplying 3,3', () => {
         assert.equal(multiply(3,3), 9, 'The output of multiplying 3 * 3 is wrong')
     })
+
+    it('Tests the return value of multiplying 4,4', () => {
+        assert.equal(multiply(4,4), 9, 'The output of multiplying 4 * 4 is wrong')
+    })
 })
